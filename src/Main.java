2299003416fedf0b4b50ace1bf8b76d49da10b30
Vincent Te Tau <vincent.tetau@gmail.com>
2016-06-11 17:43:53 +0100

    - Support set generation - Extra games

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 import lpsolve.LpSolve;
 import lpsolve.LpSolveException;
 
+import java.util.Iterator;
+
 /**
  * Created by Vincent on 2/06/2016.
  */
@@ -12,14 +14,18 @@ public class Main {
         Game game = Game.createGameFromJSON(json);
         printGameDetails(game);
 
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("Args " + args[i]);
-        }
+//        for (int i = 0; i < args.length; i++) {
+//            System.out.println("Args " + args[i]);
+//        }
+
+        if (args.length == 1)
+            printSupportSets(game);
 
         try {
-            LpSolve lpSolve = LpSolve.makeLp(0, game.getActions()[0].length);
+            LpSolve lpSolve = LpSolve.makeLp(game.getActions().length, game.getActions()[0].length);
+
             //lpSolve.strAddConstraint();
-        } catch (LpSolveException e){
+        } catch (LpSolveException e) {
             e.printStackTrace();
         }
     }
@@ -27,6 +33,34 @@ public class Main {
     public static void printGameDetails(Game game) {
         for (int i = 0; i < game.getAgents().size(); i++) {
             System.out.printf("Agent %d: %s%n", i + 1, game.getAgents().get(i));
+        }
+    }
+
+    public static String[][] generateSupportSets(Game game, int player) {
+        String[] actions = game.getActions()[player];
+        String[][] supportSets = Util.generateCombinationsProper(actions);
+        return supportSets;
+    }
+
+    public static void printSupportSets(Game game) {
+        String[][] supportSetsPlayer1 = generateSupportSets(game, 0);
+        String[][] supportSetsPlayer2 = generateSupportSets(game, 1);
+        for (int i = 0; i < supportSetsPlayer1.length; i++) {
+            System.out.printf("Player 1 support set (%d): [", i + 1);
+            for (int j = 0; j < 2; j++) {
+                System.out.print(supportSetsPlayer1[i][j]);
+                if (j == 0) System.out.print(", ");
+            }
+            System.out.println("]");
+        }
+
+        for (int i = 0; i < supportSetsPlayer2.length; i++) {
+            System.out.printf("Player 2 support set (%d): [", i + 1);
+            for (int j = 0; j < 2; j++) {
+                System.out.print(supportSetsPlayer2[i][j]);
+                if (j == 0) System.out.print(", ");
+            }
+            System.out.println("]");
         }
     }
 
--- src/Util.java
+++ src/Util.java
@@ -6,13 +6,27 @@ import java.nio.file.Files;
  * Created by Vincent on 2/06/2016.
  */
 public class Util {
-    public static String readFileToString(String fileName){
+    public static String readFileToString(String fileName) {
         String output = "";
         try {
             output = new String(Files.readAllBytes(FileSystems.getDefault().getPath(fileName)));
-        } catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return output;
+    }
+
+    public static String[][] generateCombinationsProper(String[] input) {
+        String[][] result = new String[input.length * input.length][input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                int position = (i * input.length) + j;
+                result[position][0] = input[i];
+                result[position][1] = input[j];
+            }
+        }
+
+        return result;
     }
 }
